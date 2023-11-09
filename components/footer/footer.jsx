e06660ab6602c0bf7ae3f75abbc2684d70d6a5b0
Sagarjainjain <sagarjain.jain10@gmail.com>
2023-11-09 15:22:33 +0530

    added_whats_app_link

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -1,6 +1,7 @@
 import "./footer.css";
 import Image from "next/image";
 import whatsapp from "../../assets/WhatsApp.png";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -9,11 +10,12 @@ const Footer = () => {
       <div className="footer-title">
         <h1>Connect With US</h1>
         <p>We love our customers, feel free to visit</p>
-        <div className="footer-button">
-          <Image src={whatsapp} height={20} alt="whats-app" />
-
-          <p>Message us on whatsapp</p>
-        </div>
+        <Link href="https://wa.link/q8syo5" style={{textDecoration: "None"}}>
+          <div className="footer-button">
+            <Image src={whatsapp} height={20} alt="whats-app" />
+            <p>Message us on whatsapp</p>
+          </div>
+        </Link>
       </div>
     </div>
   );
